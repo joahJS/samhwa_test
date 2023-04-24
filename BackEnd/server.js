5fb406e8ad59bb3d0bea9b94707495979e266fce
joahJS <91927771+joahJS@users.noreply.github.com>
2023-04-24 17:41:22 +0900

    515125

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -2,12 +2,17 @@
 import path from "path";
 
 // const express = require('express');
-import express from 'express';
+import express, { response } from 'express';
 import nunjucks from 'nunjucks';
 //import bodyParser from "body-parser";
 
 import mysql from 'mysql';
-const conn = {
+import bodyParser from "body-parser";
+
+const app = express();
+const __dirname = path.resolve();
+
+const dbconfig = {
     host: '127.0.0.1',
     port: '3306',
     user: 'root',
@@ -15,34 +20,30 @@ const conn = {
     database: 'test_board',
 }
 
-let connection = mysql.createConnection(conn);  //DB 커넥션 생성
+let connection = mysql.createConnection(dbconfig);  //DB 커넥션 생성
 connection.connect(); //DB 접속
 
 //let sql = "INSERT INTO `board` (`B_TITLE`, `USER_ID`) VALUES ('for test01', 'test texts02')";
 
-connection.query(sql, function(err, results, fields) {
-    if (err) {
-        console.log(err);
-    }
-    console.log(results);
-});
+//connection.query(sql, function(err, results, fields) {
+//    if (err) {
+//        console.log(err);
+//    }
+//    console.log(results);
+//});
 
-sql = "SELECT * FROM board";
+//sql = "SELECT * FROM board";
 
-connection.query(sql, function (err, results, fields) {
-    if (err) {
-        console.log(err);
-    }
-    console.log(results);
-});
+//connection.query(sql, function (err, results, fields) {
+//    if (err) {
+//        console.log(err);
+//    }
+//    console.log(results);
+//});
 
-connection.end();
-
-
+// Read
 
 
-const app = express();
-const __dirname = path.resolve();
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/view/index.html");
@@ -55,5 +56,31 @@ app.listen(3000, function() {
 app.set('view engine', 'html');
 nunjucks.configure('view', {express: app});
 
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(`${__dirname}/public`));
+
+// Create
+
+app.get('/write', (req, res) => {
+    res.render('write.html');
+});
+
+//app.get('/list', (req, res) => {
+//    res.render('list.html');
+//});
+
+app.post('/create', (req, res) => {
+    console.log(req.body)
+})
+
+//connection.end();
+
+app.get('/list', (req, res) => {
+    
+    const readSql = "select * from board"
+    connection.query(readSql, function (err, results, fields) {
+        if (err) throw err;
+        res.send(results)
+    });
+    
+});
